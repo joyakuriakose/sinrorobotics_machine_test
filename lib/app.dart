20e0f637cadf576601ade963bc863df1666a780d
joyak <joyaajish8695@gmail.com>
2025-08-16 10:41:10 +0530

    edit update cleared

--- lib/app.dart
+++ lib/app.dart
@@ -2,12 +2,8 @@
 import 'models/product_list_model.dart';
 
 abstract class App{
-  static const String appTitle = "Smart Taurus Marketing App";
+  static const String appTitle = "Marketing App";
   static const String appVersionName = "1.0.0";
-  static String token = '';
-  // static EmployeeDetails user = EmployeeDetails();
-  // static List<Shop> cusdetails = [];
-  // static List<ShopShop> shop = [];
    static List<ProductListing> prodDetails = [];
 
 
